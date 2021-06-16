c79fb3e75df00a311298e21bd5167715d8c2b042
Chrispong Lee <32980495+cl21484952@users.noreply.github.com>
2021-06-16 16:49:00 +0700

    Changelog - keep only exercise related code

--- src/index.js
+++ src/index.js
@@ -32,15 +32,6 @@ app.get("/notepad/:id", (req, res) => {
   // - Find note with given noteId
   // - If not found, return HTTP Code 404 and "Not Found"
   // - Return HTTP Code 200 and the note if found
-  const noteId = parseInt(req.params.id)
-  const foundNote = currentNote.find((note) => {
-    return note.id === noteId
-  })
-  if (!foundNote) {
-    res.status(404).send("Not Found")
-    return
-  }
-  res.status(200).json(foundNote)
 })
 
 app.post("/notepad", (req, res) => {
@@ -51,22 +42,6 @@ app.post("/notepad", (req, res) => {
   // - Check if "note" attribute is type of string
   //   - If missing, return HTTP Code 400 and "note property is not string!"
   // - Then create a new note object and pust in to currentNote variable
-  const rawJson = req.body
-  if (!rawJson.note) {
-    res.status(400).send("Missing body property <note>!")
-    return
-  }
-  if (!(rawJson.note instanceof String)) {
-    res.status(400).send("note property is not string!")
-    return
-  }
-  const noteObj = {
-    id: Math.floor(Math.random() * 2 ** 31),
-    note: rawJson.note,
-  }
-  currentNote.push(noteObj)
-
-  res.status(201).send("OK")
 })
 
 // Task 3: Delete note from currentNote
@@ -76,22 +51,6 @@ app.post("/notepad", (req, res) => {
 // - Given ID
 // - Find the note, if not found, return HTTP Code 404 and "No such Note!"
 // - If Found, remove the note from the array
-app.delete("/notepad/:id", (req, res) => {
-  const noteId = parseInt(req.params.id)
-
-  const foundNote = currentNote.find((note) => {
-    return note.id === noteId
-  })
-  if (!foundNote) {
-    res.status(404).send("No such Note!")
-    return
-  }
-
-  currentNote = currentNote.filter((note) => {
-    return note.id !== noteId
-  })
-  res.status(200).send("OK")
-})
 
 // Task 4: clear all notes
 // Requirement:
@@ -99,10 +58,6 @@ app.delete("/notepad/:id", (req, res) => {
 // - Path: /notepad
 // - HTTP Method: DELETE
 // - HTTP Code 200 when currentNote is set to empty list
-app.delete("/notepad/:id", (req, res) => {
-  currentNote = []
-  res.status(200).send("OK")
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
